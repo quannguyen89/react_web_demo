fae13fc972d04dd570548d0657c35e9d58476ec1
Nguyen Anh Quan <tolo@Nguyens-Mac-Pro.local>
2015-08-02 11:08:40 +0700

    Install additional packages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,5 @@ import React from 'react';
 import Router from 'react-router';
 import routes from './routes';
 
+require('bootstrap/dist/css/bootstrap.min.css');
 Router.run(routes, Handler => React.render(<Handler />, document.body));
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
 module.exports.getConfig = function(type) {
 
-  var isDev = type === 'development';
+    var isDev = type === 'development';
 
-  var config = {
-    entry: './app/scripts/main.js',
-    output: {
-      path: __dirname,
-      filename: 'main.js'
-    },
-    debug : isDev,
-    module: {
-      loaders: [{
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }]
+    var config = {
+        entry: './app/scripts/main.js',
+        output: {
+            path: __dirname,
+            filename: 'main.js'
+        },
+        debug: isDev,
+        module: {
+            loaders: [{
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+                {test: /\.(svg|eot|woff|ttf|png|ico|woff2)(\?.*)?$/, loader: 'file-loader'},
+                {test: /\.css$/, loader: 'style-loader!css-loader'},
+                {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}
+
+            ]
+        }
+    };
+
+    if (isDev) {
+        config.devtool = 'eval';
     }
-  };
 
-  if(isDev){
-    config.devtool = 'eval';
-  }
-
-  return config;
-}
+    return config;
+};
