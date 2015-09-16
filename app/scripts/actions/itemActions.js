b2047bb950b496581771cf19e09ae7aeb8f03498
Nguyen Anh Quan <tolo@Nguyens-Mac-Pro.local>
2015-09-16 12:54:12 +0700

    integrate with beego

--- app/scripts/actions/itemActions.js
+++ app/scripts/actions/itemActions.js
@@ -13,13 +13,13 @@ ItemActions.loadItems.preEmit = function(data){
   // make your api call/ async stuff here
   // we use setTimeout for faking async behaviour here
   // Need to define api with port 1221
-  $.getJSON('http://localhost:1221/house', null, ItemActions.loadItemsSuccess);
+  $.getJSON('http://localhost:8080/house', null, ItemActions.loadItemsSuccess);
 };
 
 ItemActions.loadHouseDetail.preEmit = function(data){
   // make your api call/ async stuff here
   // we use setTimeout for faking async behaviour here
-  $.getJSON('http://localhost:1221/house/1', null, ItemActions.loadHouseDetailSuccess);
+  $.getJSON('http://localhost:8080/house?id=1', null, ItemActions.loadHouseDetailSuccess);
 };
 
 export default ItemActions;
